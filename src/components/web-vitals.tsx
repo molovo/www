5062a6d502f84a45c6d7dabb4a43aad2219f3612
James Dinsdale <hi@molovo.co>
2024-12-13 01:31:03 +0000

    Fix web vitals reporting code

--- src/components/web-vitals.tsx
+++ src/components/web-vitals.tsx
@@ -1,35 +1,47 @@
 'use client'
 
-import { useEventListener } from '@superrb/react-addons/hooks'
 import { Metric } from 'web-vitals'
 import { useReportWebVitals } from 'next/web-vitals'
 import { v4 as uuidv4 } from 'uuid'
 import { usePathname } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { useEventListener } from '@superrb/react-addons/hooks'
 
-const queue: Set<Metric> = new Set()
+const queue = new Map<string, Metric>()
 
 const flushQueue = () => {
   if (queue.size === 0) {
     return
   }
 
-  const uuid = uuidv4()
+  return new Promise<void>((resolve) => {
+    navigator.sendBeacon(
+      'https://talon.molovo.co/api/measure/deb3c781-f593-4caa-9149-f2eb9c73cdf0',
+      JSON.stringify([...queue.values()]),
+    )
 
-  navigator.sendBeacon(
-    'https://talon.molovo.co/api/measure/deb3c781-f593-4caa-9149-f2eb9c73cdf0',
-    JSON.stringify([...queue].map((metric) => ({ ...metric, id: uuid }))),
-  )
+    queue.clear()
 
-  queue.clear()
+    resolve()
+  })
 }
 
 const WebVitals = () => {
+  const [id, setId] = useState<string>(uuidv4())
   const pathname = usePathname()
 
   useReportWebVitals((metric) => {
-    queue.add({ ...metric, url: window.location.href })
+    queue.set(metric.name, {
+      ...metric,
+      id,
+      url: pathname,
+      domain: window.location.hostname,
+    })
   })
+
+  useEffect(() => {
+    setId(uuidv4())
+  }, [pathname])
 
   useEventListener(
     'visibilitychange',
