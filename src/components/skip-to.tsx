2c41a4cacdbfd82a2c410ba1410169c7ad0a039e
James Dinsdale <hi@molovo.co>
2025-05-22 21:47:56 +0100

    Use correct aria landmark for a11y links

--- src/components/skip-to.tsx
+++ src/components/skip-to.tsx
@@ -6,12 +6,12 @@ const SkipTo = () => {
   const { open: openNav } = useNavStateStore()
 
   return (
-    <div className="skip-to" id="skip-to">
+    <nav className="skip-to" id="skip-to">
       <a href="#content">Skip to Content</a>
       <a href="#nav" onClick={openNav}>
         Skip to Navigation
       </a>
-    </div>
+    </nav>
   )
 }
 
