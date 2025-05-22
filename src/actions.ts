f83e842824ed4d1d4b324f0e99c81b3d511f4145
James Dinsdale <hi@molovo.co>
2025-05-22 21:43:03 +0100

    Fix contact form action

--- src/actions.ts
+++ src/actions.ts
@@ -1,24 +1,20 @@
 'use server'
 
-import { NextResponse } from 'next/server'
 import { ServerClient } from 'postmark'
 import { MessageSendingResponse } from 'postmark/dist/client/models'
 
-interface FormData {
-  message: string
-  email: string
-}
-
 export const contactSubmit = async (data: FormData) => {
   const client = new ServerClient(process.env.POSTMARK_API_KEY as string)
+
+  console.log(data.get('message'), data.get('email'))
 
   try {
     const response: MessageSendingResponse = await client.sendEmail({
       To: process.env.CONTACT_FORM_EMAIL_TO as string,
       From: process.env.CONTACT_FORM_EMAIL_FROM as string,
-      ReplyTo: data.email,
+      ReplyTo: data.get('email') as string,
       Subject: 'Contact form submission from molovo.co',
-      TextBody: data.message,
+      TextBody: data.get('message') as string,
       MessageStream: 'outbound',
     })
   } catch (err) {
