565b62684e53a626ccbddaf3c375000071c828d2
James Dinsdale <hi@molovo.co>
2025-06-05 21:20:27 +0100

    New blog post

--- src/components/article/perpetually-online/perpetually-online-heading.tsx
+++ src/components/article/perpetually-online/perpetually-online-heading.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { cinema } from '@/fonts/articles/the-world-is-broken'
+import { cinema } from '@/fonts/articles/perpetually-online'
 import ContentImage from '@/components/content-image'
-import laptop from '@/images/articles/the-world-is-broken/laptop.png'
+import laptop from '@/images/articles/perpetually-online/laptop.png'
 import Sun from './icons/sun.svg'
 import Moon from './icons/moon.svg'
 import Lamp from './icons/lamp.svg'
@@ -10,6 +10,7 @@ import useHeaderStyle from '@/hooks/use-header-style'
 
 const PerpetuallyOnlineHeading = () => {
   const setRef = useHeaderStyle('red')
+
   return (
     <div className="perpetually-online-heading" ref={setRef}>
       <div className="perpetually-online-heading__animation">
@@ -22,7 +23,7 @@ const PerpetuallyOnlineHeading = () => {
         src={laptop}
         sizes="24em"
       />
-      <h2 className={cinema.className}>Perpetually Online</h2>
+      <h1 className={cinema.className}>Perpetually Online</h1>
     </div>
   )
 }
