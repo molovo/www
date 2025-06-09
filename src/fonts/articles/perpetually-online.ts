f38b0e9845335901fdc2507e1f9f7ff2119c2a8c
James Dinsdale <hi@molovo.co>
2025-06-09 17:12:57 +0100

    Remove unused fonts

--- src/fonts/articles/perpetually-online.ts
+++ src/fonts/articles/perpetually-online.ts
@@ -1,10 +1,5 @@
 import localFont from 'next/font/local'
 
-export const area51 = localFont({
-  src: '../../fonts/Area51.woff2',
-  display: 'swap',
-})
-
 export const cinema = localFont({
   src: '../../fonts/Cinema.woff2',
   display: 'swap',
